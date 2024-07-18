2d756fb05672bd3d3886a341018e764b7a9d3eb6
FrontLAB <123484134+Frontlab-kr@users.noreply.github.com>
2024-07-18 23:29:40 +0900

    퍼블시작

---
+++ assets/js/mobile/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // 현재 페이지의 URL을 가져옴
+  var url = window.location.pathname;
+  // URL에서 파일 이름을 추출
+  var fileName = decodeURIComponent(url.substring(url.lastIndexOf('/') + 1));
+  // 파일 이름이 비어 있으면 기본 파일 이름으로 index.html 설정
+  if (!fileName) {
+    fileName = 'index.html';
+  }
+  // title 태그에 파일 이름 설정
+  $('title').text(fileName);
+});
+
+$(document).ready(function () {
+  $('#header').load('../components/mobile/header.html');
+  $('#footer').load('../components/mobile/footer.html');
+  $('#menu').load('../components/mobile/menu.html');
+});
+
+var stateMenu = false;
+function menuOpen() {
+  stateMenu = true;
+  $('.mandom').addClass('stateMenu');
+}
+function menuClose() {
+  stateMenu = true;
+  $('.mandom').removeClass('stateMenu');
+}
