58906e2441855d671651a295c0f82e231a72383e
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2024-10-21 11:34:51 +0900

    js

--- assets/js/desktop/common.js
+++ assets/js/desktop/common.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
-  // 현재 페이지의 URL을 가져옴
-  var url = window.location.pathname;
-  // URL에서 파일 이름을 추출
-  var fileName = decodeURIComponent(url.substring(url.lastIndexOf('/') + 1));
-  // 파일 이름이 비어 있으면 기본 파일 이름으로 index.html 설정
-  if (!fileName) {
-    fileName = 'index.html';
-  }
-  // title 태그에 파일 이름 설정
-  $('title').text(fileName);
-});
+// $(document).ready(function () {
+//   // 현재 페이지의 URL을 가져옴
+//   var url = window.location.pathname;
+//   // URL에서 파일 이름을 추출
+//   var fileName = decodeURIComponent(url.substring(url.lastIndexOf('/') + 1));
+//   // 파일 이름이 비어 있으면 기본 파일 이름으로 index.html 설정
+//   if (!fileName) {
+//     fileName = 'index.html';
+//   }
+//   // title 태그에 파일 이름 설정
+//   $('title').text(fileName);
+// });
 
-$(document).ready(function () {
-  $('#mandom-header').load('../components/desktop/header.html');
-  $('#mandom-footer').load('../components/desktop/footer.html');
-  $('#mandom-gnb').load('../components/desktop/gnb.html');
-});
+// $(document).ready(function () {
+//   $('#mandom-header').load('../components/desktop/header.html');
+//   $('#mandom-footer').load('../components/desktop/footer.html');
+//   $('#mandom-gnb').load('../components/desktop/gnb.html');
+// });
 
 var stateMenu = false;
 function menuOpen() {
--- assets/js/mobile/common.js
+++ assets/js/mobile/common.js
@@ -1,22 +1,22 @@
-$(document).ready(function () {
-  // 현재 페이지의 URL을 가져옴
-  var url = window.location.pathname;
-  // URL에서 파일 이름을 추출
-  var fileName = decodeURIComponent(url.substring(url.lastIndexOf('/') + 1));
-  // 파일 이름이 비어 있으면 기본 파일 이름으로 index.html 설정
-  if (!fileName) {
-    fileName = 'index.html';
-  }
-  // title 태그에 파일 이름 설정
-  $('title').text(fileName);
-});
+// $(document).ready(function () {
+//   // 현재 페이지의 URL을 가져옴
+//   var url = window.location.pathname;
+//   // URL에서 파일 이름을 추출
+//   var fileName = decodeURIComponent(url.substring(url.lastIndexOf('/') + 1));
+//   // 파일 이름이 비어 있으면 기본 파일 이름으로 index.html 설정
+//   if (!fileName) {
+//     fileName = 'index.html';
+//   }
+//   // title 태그에 파일 이름 설정
+//   $('title').text(fileName);
+// });
 
-$(document).ready(function () {
-  $('#mandom-header').load('../components/mobile/header.html');
-  $('#mandom-footer').load('../components/mobile/footer.html');
-  $('#mandom-menu').load('../components/mobile/menu.html');
-  $('#mandom-gnb').load('../components/mobile/gnb.html');
-});
+// $(document).ready(function () {
+//   $('#mandom-header').load('../components/mobile/header.html');
+//   $('#mandom-footer').load('../components/mobile/footer.html');
+//   $('#mandom-menu').load('../components/mobile/menu.html');
+//   $('#mandom-gnb').load('../components/mobile/gnb.html');
+// });
 
 var stateMenu = true;
 function menuOpen() {
@@ -27,6 +27,3 @@ function menuClose() {
   stateMenu = true;
   $('body').removeClass('stateMenu');
 }
-$(document).ready(function () {
-  //
-});
